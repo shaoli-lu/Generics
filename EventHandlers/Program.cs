b61b57ac389e002ba52eed23f68f936e9c6cfd60
Shaoli Lu <shaoli_lu@yahoo.co>
2022-04-03 12:23:33 -0500

    Event handlers

---
+++ EventHandlers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Timers;
+
+namespace EventHandler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer myTimer = new Timer(2000);
+            myTimer.Elapsed += MyTimer_Elapsed;
+            myTimer.Elapsed += MyTimer_Elapsed1;
+
+            myTimer.Start();
+
+            Console.WriteLine("Press Enter to remove the red event.");
+            Console.ReadLine();
+
+            myTimer.Elapsed -= MyTimer_Elapsed;
+
+            Console.ReadLine();
+        }
+
+        private static void MyTimer_Elapsed1(object sender, ElapsedEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Elapsed: {0:HH:mm:ss.fff}", e.SignalTime);
+
+        }
+
+        private static void MyTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Elapsed: {0:HH:mm:ss.fff}", e.SignalTime);
+        }
+    }
+}
